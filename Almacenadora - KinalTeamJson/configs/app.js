fbf95296aa9ba00e2a876b42337ac3a9ad9c356e
jcruz-2018059 <jcruz-2018059@kinal.edu.gt>
2023-04-17 16:29:37 -0600

    Se creó el proyecto inicial

---
+++ Almacenadora - KinalTeamJson/configs/app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const express = require('express');
+//Logs de las solicitudes que reciba el servidor
+const morgan = require('morgan');
+//Seguridad básica al servidor
+const helmet = require('helmet');
+//Aceptación de solicitudes desde otro origen o desde la misma máquina
+const cors = require('cors');
+//Instancia de express
+const app = express();
+const port = process.env.PORT || 3200;
+
+//Configurar el servidor de express
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+//Función para levanter el puerto
+exports.initServer = ()=>{
+    app.listen(port);
+    console.log(`Server http running in port ${port}`);
+}
