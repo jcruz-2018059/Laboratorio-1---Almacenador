24bf3196760a0a24a9b0964c28dc972a755e7b3c
Eduardo <ecruz-2021122@kinal.edu.gt>
2023-04-21 09:16:28 -0600

    Se añadió la función para crear el administrador por defecto.

--- Almacenadora - KinalTeamJson/src/user/user.controller.js
+++ Almacenadora - KinalTeamJson/src/user/user.controller.js
@@ -1,8 +1,33 @@
 'use strict'
 
 const User = require('./user.model');
+const { encrypt } = require('../../utils/validate');
 
 
 exports.test = (req, res)=>{
     return res.send({message: 'Test function is running :)'});
+}
+
+exports.default = async()=>{
+    try{
+        let defAdmin = {
+            name: 'General',
+            surname: 'Administrator',
+            username: 'admin',
+            password: 'admin',
+            email: 'admin@storage.gt',
+            phone: '+502 5984 2658',
+            role: 'ADMIN'
+        }
+        let existAdministrator = await User.findOne({username: 'admin'});
+        if(existAdministrator){
+            return console.log('Default admin is already created.');
+        }
+        defAdmin.password = await encrypt(defAdmin.password);
+        let createDefaultAdmin = new User(defAdmin);
+        await createDefaultAdmin.save();
+        return console.log('Default administrator created.');
+    }catch(err){
+        console.error(err);
+    }
 }
