514d92d42be385f0f4ce3df8044e03d7804b8f6d
Javier <jcruz-2018059@kinal.edu.gt>
2023-04-21 10:13:47 -0600

    Se agregaron las funciones al controllador Cliente

--- Almacenadora - KinalTeamJson/src/client/client.controller.js
+++ Almacenadora - KinalTeamJson/src/client/client.controller.js
@@ -3,3 +3,56 @@
 const Client = require('./client.model');
 const e = require('express');
 
+exports.test = (req, res)=>{
+    return res.send({message: 'Test funtion is running'});
+}
+
+exports.addClient = async(req, res)=>{
+    try{
+        let data = req.body;
+        let client = await Client.findOne({dpi: data.dpi});
+        if(client) return res.send({message: 'Client already create'})
+        let newClient = new Client(data)
+        await newClient.save();
+        return res.status(201).send({message: 'Client created succesfully'})
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error server, error saving client'})
+    }
+};
+
+exports.getClient = async(req, res)=>{
+    try{
+        let clientId = req.params.id;
+        let client = await Client.findOne({_id:clientId});
+        if(!client) return res.send({message: 'Client not found'})
+        return res.send({message: 'Client found:', client})
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error searching Client'})
+    }
+}
+
+exports.getClients = async(req, res)=>{
+    try{
+        let categories = Client.find()
+        return res.send({categories});
+    }catch(err){
+        console.error(err);
+        return res.status(500).send({message: 'Error server, clients not found'})
+    }
+}
+
+/*exports.updateClient = async(req, res)=>{
+    try{
+        let clientId = req.params.id;
+        let data = req.body;
+        let existClient = Client.findOne({_id: clientId})
+        if(existClient){
+            if(data.dpi !=)
+        }
+    }catch(err){
+        console.error(err)
+        return res.status(500).send({message: 'Error server, Client not updated'})
+    }
+}*/
