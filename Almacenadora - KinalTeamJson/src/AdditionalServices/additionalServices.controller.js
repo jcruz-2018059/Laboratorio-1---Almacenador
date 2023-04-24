905c2bd9ead2a0d120441943d2c3088b9654ccf5
Informatica Kinal <informatica@ALUMNOS.local>
2023-04-24 09:45:38 -0600

    Add Additional Services

--- Almacenadora - KinalTeamJson/src/AdditionalServices/additionalServices.controller.js
+++ Almacenadora - KinalTeamJson/src/AdditionalServices/additionalServices.controller.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const AdditionalSevie = require('./additionalServices.model');
+const AdditionalServies = require('./additionalServices.model');
 
 exports.test = (req, res)=>{
     res.send({message: 'Test fuction is running'});
@@ -8,7 +8,10 @@ exports.test = (req, res)=>{
 
 exports.addAdditionalServices = async(req, res)=>{
     try{
-
+        let data = req.body;
+        let additionalService = new AdditionalServies(data);
+        await additionalService.save();
+        return res.send({message: 'Service created successfully', additionalService});
     }catch(err){
         console.error(err);
         return res.status(500).send({message: 'Error crea'})
--- Almacenadora - KinalTeamJson/src/AdditionalServices/additionalServices.routes.js
+++ Almacenadora - KinalTeamJson/src/AdditionalServices/additionalServices.routes.js
@@ -5,5 +5,6 @@ const express = require('express');
 const api = express.Router();
 
 api.get('/', additionalServieController.test);
+api.post('/addServices', additionalServieController.addAdditionalServices);
 
 module.exports = api;
